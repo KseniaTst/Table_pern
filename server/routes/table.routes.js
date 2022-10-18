c5b6b65ab9d95d44d9cf74fb3a89be4fd71fc3da
Ksenia Tst <kseniatsytrikova@gmail.com>
2022-10-18 20:17:41 +0300

    replace server files out of directory to deploy on heroku and configure project for deployment

--- server/routes/table.routes.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const tableController = require('../controllers/table.controller')
-
-router.get('/get', tableController.getTable)
-router.get('/getByName', tableController.getByName)
-router.get('/sort', tableController.Sort)
-
-module.exports = router
